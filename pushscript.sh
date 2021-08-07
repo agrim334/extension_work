aa6ae4c7f130ed5eb1a7689ffcc8542efbe3f780
agrim334 <agrim334@gmail.com>
2021-08-07 19:40:10 +0530

    halfway done through python

---
+++ pushscript.sh
@@ -0,0 +1,5 @@
+git add .
+git rm --cached Python_compare/all_projects/ -r
+git rm --cached Python_compare/selected/XSStrike/ -r
+git rm --cached Python_compare/selected/PythonPlantsVsZombies/ -r
+git rm --cached Python_compare/selected/rebound/ -r
