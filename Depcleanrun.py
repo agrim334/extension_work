0160cfc2a72062d63eeab8f5eeba357a24b484b6
agrim334 <agrim334@gmail.com>
2021-08-07 11:26:56 +0530

    Updated Results.docx

--- Depcleanrun.py
+++
@@ -1,8 +0,0 @@
-import p2
-import math
-import sklearn
-import sys
-import p1
-
-p2.f2()
-print(math.ceil(9.2))
--- Python_compare/all_projects/rebound/rebound/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from rebound import main
-
-if __name__ == "__main__":
-    main()
