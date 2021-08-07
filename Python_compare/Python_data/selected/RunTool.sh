c1746c09f32d987748c1abb5184931ee5c661252
agrim334 <agrim334@gmail.com>
2021-08-07 10:59:49 +0530

    changed method of Response time calculation for python

--- Python_compare/Python_data/selected/RunTool.sh
+++ Python_compare/Python_data/selected/RunTool.sh
@@ -2,7 +2,8 @@ for i in `ls -d */`
 do
 	for j in `find $i -type f -name "*.py"`
 	do
-		`echo $j >> PC_res.txt && (time pycln -a -d $j) &>> PC_res.txt`
-		`echo $j >> AF_res.txt && (time autoflake --remove-all-unused-imports --ignore-init-module-imports --recursive $j) &>> AF_res.txt`
+		`echo $j >> PC_res.txt`
+		`echo $j >> AF_res.txt` 
+		python3 RunImpCheck.py -f $j
 	done
 done
