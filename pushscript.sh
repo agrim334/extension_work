8f7713b1f4c1837231ed51f27f9ac1875bf9b0d0
agrim334 <agrim334@gmail.com>
2021-08-08 16:52:58 +0530

    Added accuracy metrics for python tools

--- pushscript.sh
+++ pushscript.sh
@@ -3,3 +3,6 @@ git rm --cached Python_compare/all_projects/ -r
 git rm --cached Python_compare/selected/XSStrike/ -r
 git rm --cached Python_compare/selected/PythonPlantsVsZombies/ -r
 git rm --cached Python_compare/selected/rebound/ -r
+read com_var
+git commit -m "$com_var"
+git push git@github.com:agrim334/extension_work.git
